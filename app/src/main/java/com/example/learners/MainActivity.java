6f4b18fecf8743bfe1116f191ade345a79e5a59a
Shekhar Shinde <shekharshinde9990@gmail.com>
2021-05-23 20:36:24 +0530

    removed files

--- app/src/main/java/com/example/learners/MainActivity.java
+++ app/src/main/java/com/example/learners/MainActivity.java
@@ -12,14 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Handler handler= new Handler();
-        new Handler().postDelayed(new Runnable(){
-            @Override
-            public void run() {
-                Intent intent = new Intent(MainActivity.this,LoginActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        },2500);
+        
     }
 }
